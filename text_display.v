deb66041c0531dbf8e1aab72c14d15e44839f8a0
bear1222 <86869648+bear1222@users.noreply.github.com>
2023-12-30 01:08:59 +0800

    upload text display 1229

--- text_display.v
+++ text_display.v
@@ -2,9 +2,11 @@ module text_display(
     input clk,
     input valid,
     input [9:0] vgax, vgay,
-    input [9:0] sx, sy,  
+    input [9:0] sx, sy, ex, ey, 
     input [124:0] text, // only alphabet
-    output [11:0] pixel
+    input [2:0] font_color, 
+    input [2:0] background_color,
+    output [2:0] pixel
 );
     wire [10:0] addr;
     wire [7:0] data;
@@ -15,14 +17,86 @@ module text_display(
     wire [3:0] char_row;
     wire [2:0] char_bit;
     assign id = (vgay - sy) >> 3;
-    assign ascii = (id < 25 ? text[id * 5 +: 5] + 65 : 32); // type[typeid] + 65
+    assign ascii = (id < 25 && text[id * 5 +: 5] > 0 ? text[id * 5 +: 5] + 64 : 32); // type[typeid] + 65
     assign char_row = (vgax - sx) & 4'b1111;
     assign char_bit = (vgay - sy) & 3'b111;
 
     assign addr = {ascii, char_row};
 
-    wire xybit, biton;
+    wire xybit;
     assign xybit = data[~char_bit];
-    assign biton = valid && vgax >= sx && vgay >= sy ? xybit : 0;
-    assign pixel = biton ? 12'h000 : 12'hFFF;
+    wire show;
+    assign show = valid && vgax >= sx && vgay >= sy && vgax < ex && vgay < ey;
+    assign pixel = show ? (xybit ? font_color : background_color) : 0;
+endmodule
+
+module dict_display(
+    input clk,
+    input valid,
+    input [9:0] vgax, vgay,
+    input [9:0] sx, sy, ex, ey, 
+    input [124:0] text, // only alphabet
+    input [5:0] correct, tot, 
+    input [2:0] font_color, 
+    input [2:0] background_color,
+    output [2:0] pixel
+);
+    wire [10:0] addr;
+    wire [7:0] data;
+    ascii_rom rom(.clk(clk), .addr(addr), .data(data));
+
+    wire [9:0] id;
+    wire [6:0] ascii;
+    wire [3:0] char_row;
+    wire [2:0] char_bit;
+    assign id = (vgay - sy) >> 3;
+    assign ascii = (id < 25 && text[id * 5 +: 5] > 0 ? text[id * 5 +: 5] + 64 : 32); // type[typeid] + 65
+    assign char_row = (vgax - sx) & 4'b1111;
+    assign char_bit = (vgay - sy) & 3'b111;
+
+    assign addr = {ascii, char_row};
+
+    wire xybit;
+    assign xybit = data[~char_bit];
+    wire show;
+    assign show = valid && vgax >= sx && vgay >= sy && vgax < ex && vgay < ey;
+    assign pixel = show ? (xybit ? font_color : (id < tot ? (id < correct ? 3 : 2) : background_color)) : 0;
+endmodule
+
+module num_display(
+    input clk,
+    input valid,
+    input [9:0] vgax, vgay,
+    input [9:0] sx, sy, ex, ey, 
+    input [9:0] num, // 0~1000
+    input [2:0] dot, 
+    input [2:0] font_color, 
+    input [2:0] background_color,
+    output [2:0] pixel
+);
+    wire [10:0] addr;
+    wire [7:0] data;
+    wire [11:0] nums; // 123 -> 0~3: 1, 4~7: 2, 8~11: 3
+    wire [3:0] a, b, c;
+    assign a = num / 100; assign b = num / 10 % 10; assign c = num % 10;
+    assign nums = {a, b, c};
+    /////
+    ascii_rom rom(.clk(clk), .addr(addr), .data(data));
+
+    wire [9:0] id;
+    wire [6:0] ascii;
+    wire [3:0] char_row;
+    wire [2:0] char_bit;
+    assign id = (vgay - sy) >> 3;
+//    assign ascii = (id < 25 && text[id * 5 +: 5] > 0 ? text[id * 5 +: 5] + 64 : 32); // type[typeid] + 65
+    assign char_row = (vgax - sx) & 4'b1111;
+    assign char_bit = (vgay - sy) & 3'b111;
+
+    assign addr = {ascii, char_row};
+
+    wire xybit;
+    assign xybit = data[~char_bit];
+    wire show;
+    assign show = valid && vgax >= sx && vgay >= sy && vgax < ex && vgay < ey;
+    assign pixel = show ? (xybit ? font_color : background_color) : 0;
 endmodule
