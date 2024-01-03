f807d35effab924c1fbd9445d92ccea12e63e65d
bear1222 <86869648+bear1222@users.noreply.github.com>
2024-01-04 01:30:45 +0800

    add menu screen

---
+++ screen_menu.v
@@ -0,0 +1,266 @@
+`define titlex 80
+`define titley 100
+
+`define bestline 40
+`define bestaccline 60
+`define bestwpmline 80
+`define aveline 120
+`define aveaccline 140
+`define avewpmline 160
+`define ystart 510
+
+`define selectline 250
+`define twline 300
+`define selecty 200
+
+module menu_screen(
+    input clk, 
+    input rst, 
+    input [9:0] vgax, vgay, 
+    input valid, 
+    input mode, 
+    input [6:0] value, 
+    output [9:0] wpm_best,
+    output [9:0] wpm_average,
+    output [9:0] acc_best,
+    output [9:0] acc_average, 
+    output reg [11:0] pixel
+);
+
+    wire clk_25MHz;
+    clock_divider #(2) cd(
+        .clk(clk), 
+        .clk_div(clk_25MHz)
+    );
+
+    wire [2:0] pixel_title, pixel_id, pixel_id1, pixel_id2;
+    wire [2:0] pixel_best_text, pixel_bestacc_text, pixel_bestacc, pixel_bestwpm_text, pixel_bestwpm, pixel_bestmod;
+    wire [2:0] pixel_ave_text, pixel_aveacc_text, pixel_aveacc, pixel_avewpm_text, pixel_avewpm, pixel_avemod;
+    wire [2:0] pixel_select_text, pixel_mode_text, pixel_tw, pixel_num;
+
+    text_display title(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax >> 1), 
+        .vgay(vgay >> 1), 
+        .sx(`titlex), 
+        .ex(`titlex + 16),
+        .sy(`titley), 
+        .ey(`titley + 8 * 9),
+        .text({5'd18, 5'd5, 5'd3, 5'd1, 5'd18, 5'd5, 5'd16, 5'd25, 5'd20}), // TYPERACER
+        .font_color(4), 
+        .background_color(0),
+        .pixel(pixel_title)
+    );
+
+    // best
+    text_display besttext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestline), .ex(`bestline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 4), 
+        .text({5'd20, 5'd19, 5'd5, 5'd2}), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_best_text)
+    );
+    text_display acctext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestaccline), .ex(`bestaccline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 4), 
+        .text({5'd3, 5'd3, 5'd1}), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_bestacc_text)
+    );
+    num_display accdisplay(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestaccline), .ex(`bestaccline + 16), 
+        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
+        .num(acc_best), .dot(0), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_bestacc)
+    );
+    ascii_display mod_display(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestaccline), .ex(`bestaccline + 16), 
+        .sy(`ystart + 8 * 7), .ey(`ystart + 8 * 8), 
+        .ascii(6'h25), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_bestmod)
+    );
+    text_display wpmtext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestwpmline), .ex(`bestwpmline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 4), 
+        .text({5'd13, 5'd16, 5'd23}),
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_bestwpm_text)
+    );
+    num_display wpmdisplay(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`bestwpmline), .ex(`bestwpmline + 16), 
+        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
+        .num(wpm_best), .dot(0), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_bestwpm)
+    );
+
+    // ave
+    text_display avetext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`aveline), .ex(`aveline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 7), 
+        .text({5'd5, 5'd7, 5'd1, 5'd18, 5'd5, 5'd22, 5'd1}), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_ave_text)
+    );
+    text_display aveacctext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`aveaccline), .ex(`aveaccline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 4), 
+        .text({5'd3, 5'd3, 5'd1}), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_aveacc_text)
+    );
+    num_display aveaccdisplay(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`aveaccline), .ex(`aveaccline + 16), 
+        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
+        .num(acc_average), .dot(0), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_aveacc)
+    );
+    ascii_display avemod_display(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`aveaccline), .ex(`aveaccline + 16), 
+        .sy(`ystart + 8 * 7), .ey(`ystart + 8 * 8), 
+        .ascii(6'h25), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_avemod)
+    );
+    text_display avewpmtext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`avewpmline), .ex(`avewpmline + 16), 
+        .sy(`ystart), .ey(`ystart + 8 * 4), 
+        .text({5'd13, 5'd16, 5'd23}),
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_avewpm_text)
+    );
+    num_display avewpmdisplay(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`avewpmline), .ex(`avewpmline + 16), 
+        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
+        .num(wpm_average), .dot(0), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_avewpm)
+    );
+
+    text_display selecttext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`selectline), .ex(`selectline + 16), 
+        .sy(`selecty), .ey(`selecty + 8 * 7), 
+        .text({5'd20, 5'd3, 5'd5, 5'd12, 5'd5, 5'd19}),
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_select_text)
+    );
+    text_display modetext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`selectline), .ex(`selectline + 16), 
+        .sy(`selecty + 8 * 7), .ey(`selecty + 8 * 11), 
+        .text({5'd5, 5'd4, 5'd15, 5'd13}), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_mode_text)
+    );
+    text_display twtext(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`twline), .ex(`twline + 16), 
+        .sy(`selecty), .ey(`selecty + 8 * 5), 
+        .text(mode ? {5'd4, 5'd18, 5'd15, 5'd23} : {5'd3, 5'd13, 5'd9, 5'd20}), // mode ? "word" : "time"
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_tw)
+    );
+    num_display numdisplay(
+        .clk(clk), 
+        .valid(valid), 
+        .vgax(vgax), .vgay (vgay), 
+        .sx(`twline), .ex(`twline + 16), 
+        .sy(`selecty + 8 * 5), .ey(`selecty + 8 * 8), 
+        .num(value), .dot(0), 
+        .font_color(4), .background_color(0), 
+        .pixel(pixel_num)
+    );
+
+    find_max fm1(
+        .a1(pixel_title), 
+        .a2(pixel_best_text), 
+        .a3(pixel_bestacc_text), 
+        .a4(pixel_bestacc), 
+        .a5(pixel_bestmod), 
+        .a6(pixel_bestwpm_text), 
+        .a7(pixel_bestwpm), 
+        .a8(pixel_ave_text), 
+        .mx(pixel_id1)
+    );
+    find_max fm2(
+        .a1(pixel_id1), 
+        .a2(pixel_aveacc_text), 
+        .a3(pixel_aveacc), 
+        .a4(pixel_avemod), 
+        .a5(pixel_avewpm_text), 
+        .a6(pixel_avewpm), 
+        .a7(pixel_select_text), 
+        .a8(pixel_mode_text), 
+        .mx(pixel_id2)
+    );
+    find_max fm3(
+        .a1(pixel_id2), 
+        .a2(pixel_tw), 
+        .a3(pixel_num), 
+        .a4(0), 
+        .a5(0), 
+        .a6(0), 
+        .a7(0), 
+        .a8(0), 
+        .mx(pixel_id)
+    );
+
+    always @ (posedge clk_25MHz) begin
+        case(pixel_id)
+            0: pixel <= 12'hFFF; // white
+            1: pixel <= 12'h0FF; // blue
+            2: pixel <= 12'hF00; // red
+            3: pixel <= 12'h0F0; // green
+            4: pixel <= 12'd0;   // black
+            default: pixel <= 12'h00F; // error
+        endcase
+    end
+
+endmodule
--- vga.v
+++ vga.v
@@ -1,13 +1,3 @@
-`define typeline 370
-`define wordline 340
-`define accline 40
-`define wpmline 60
-`define ystart 510
-`define timeline 40
-`define timeystart 50
-`define carx 200
-`define cary 80
-
 module clock_divider #(
     parameter n = 27
 )(
@@ -29,70 +19,32 @@ endmodule
 module vga(
     input clk, 
     input rst, 
-    input [9:0] percent, 
+    input [9:0] percent, // game
     input [9:0] wpm, 
     input [9:0] acc, 
     input [4:0] correct, tot,
     input [14:0] times, 
-    input [124:0] type, // array
-    input [47:0] rd, // array
+    input [74:0] type, 
+    input [47:0] rd,   
+    input [1:0] state, // select
+    input mode, // menu
+    input [6:0] value, 
+    input [9:0] wpm_best,
+    input [9:0] wpm_average,
+    input [9:0] acc_best,
+    input [9:0] acc_average, 
     output [3:0] vgaRed,
     output [3:0] vgaGreen,
     output [3:0] vgaBlue,
     output hsync,
     output vsync
 );
+	parameter SELECT = 0;
+	parameter COUNTDOWN = 1;
+	parameter INGAME = 2;
+	parameter FINISH = 3;
 
-    wire [74:0] word0, word1, word2, word3, word4;
-    wire [4:0] len0, len1, len2, len3, len4;
-    wire [7:0] id0, id1, id2, id3, id4;
-
-    assign id0 = rd[0  +: 8];
-    assign id1 = rd[8 +: 8];
-    assign id2 = rd[16 +: 8];
-    assign id3 = rd[24 +: 8];
-    assign id4 = rd[32 +: 8];
-
-    dictionary dic0(
-        .id(id0), 
-        .wordnum(len0), 
-        .word(word0)
-    );
-    dictionary dic1(
-        .id(id1), 
-        .wordnum(len1), 
-        .word(word1)
-    );
-    dictionary dic2(
-        .id(id2), 
-        .wordnum(len2), 
-        .word(word2)
-    );
-    dictionary dic3(
-        .id(id3), 
-        .wordnum(len3), 
-        .word(word3)
-    );
-    dictionary dic4(
-        .id(id4), 
-        .wordnum(len4), 
-        .word(word4)
-    );
-
-    wire [9:0] st0, st1, st2, st3, st4;
-    assign st0 = 0;
-    assign st1 = st0 + len0 + 1;
-    assign st2 = st1 + len1 + 1;
-    assign st3 = st2 + len2 + 1;
-    assign st4 = st3 + len3 + 1;
-
-    wire [11:0] pixel_car;
-
-
-
-    reg [11:0] pixel;
-
-    assign {vgaRed, vgaGreen, vgaBlue} = valid ? pixel : 12'd0;
+    wire clk_25MHz, valid;
     wire [9:0] vgax, vgay;
 
     clock_divider #(2) cd(
@@ -110,245 +62,39 @@ module vga(
         .v_cnt(vgax)
     );
 
-    wire [2:0] pixel_type, pixel_w0, pixel_w1, pixel_w2, pixel_w3, pixel_w4, pixel_acc, pixel_wpm, pixel_acc_text, pixel_wpm_text, pixel_time, pixel_time_text, pixel_mod;
-    text_display type_in(
-        .clk(clk), 
+    wire [11:0] pixel_game, pixel_menu, pixel;
+    game_screen gc(
+		.clk(clk), 
+		.rst(rst), 
+		.percent(percent),
+		.wpm(wpm), 
+		.acc(acc),
+		.tot(tot), 
+		.correct(correct), 
+		.times(times), 
+		.type(type), 
+		.rd(rd), 
         .valid(valid), 
         .vgax(vgax), 
         .vgay(vgay), 
-        .sx(`typeline), 
-        .ex(`typeline + 16),
-        .sy(150), 
-        .ey(350),
-        .text(type),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_type)
+        .pixel(pixel_game)
     );
 
-    wire [9:0] sy0, sy1, sy2, sy3, sy4, ey0, ey1, ey2, ey3, ey4;
-    assign sy0 = 150 + (st0 << 3);
-    assign sy1 = 150 + (st1 << 3);
-    assign sy2 = 150 + (st2 << 3);
-    assign sy3 = 150 + (st3 << 3);
-    assign sy4 = 150 + (st4 << 3);
-    assign ey0 = sy1 - 8;
-    assign ey1 = sy2 - 8;
-    assign ey2 = sy3 - 8;
-    assign ey3 = sy4 - 8;
-    assign ey4 = 150 + ((st4 + len4) << 3);
-    dict_display w0(
+    menu_screen mc(
         .clk(clk), 
-        .valid(valid), 
+        .rst(rst), 
         .vgax(vgax), 
         .vgay(vgay), 
-        .sx(`wordline), 
-        .ex(`wordline + 16),
-        .sy(sy0), 
-        .ey(ey0),
-        .correct(correct), 
-        .tot(tot),
-        .text(word0),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_w0)
-    );
-    text_display w1(
-        .clk(clk), 
         .valid(valid), 
-        .vgax(vgax), 
-        .vgay(vgay), 
-        .sx(`wordline), 
-        .ex(`wordline + 16),
-        .sy(sy1), 
-        .ey(ey1),
-        .text(word1),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_w1)
+        .mode(mode), 
+        .value(value), 
+        .wpm_best(wpm_best),
+        .wpm_average(wpm_average),
+        .acc_best(acc_best),
+        .acc_average(acc_average), 
+        .pixel(pixel_menu)
     );
-    text_display w2(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), 
-        .vgay(vgay), 
-        .sx(`wordline), 
-        .ex(`wordline + 16),
-        .sy(sy2), 
-        .ey(ey2),
-        .text(word2),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_w2)
-    );
-    text_display w3(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), 
-        .vgay(vgay), 
-        .sx(`wordline), 
-        .ex(`wordline + 16),
-        .sy(sy3), 
-        .ey(ey3),
-        .text(word3),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_w3)
-    );
-    text_display w4(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), 
-        .vgay(vgay), 
-        .sx(`wordline), 
-        .ex(`wordline + 16),
-        .sy(sy4), 
-        .ey(ey4), 
-        .text(word4),
-        .font_color(4), 
-        .background_color(1),
-        .pixel(pixel_w4)
-    );
-    text_display acctext(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`accline), .ex(`accline + 16), 
-        .sy(`ystart), .ey(`ystart + 8 * 4), 
-        .text({5'd3, 5'd3, 5'd1}), 
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_acc_text)
-    );
-    num_display accdisplay(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`accline), .ex(`accline + 16), 
-        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
-        .num(acc), .dot(0), 
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_acc)
-    );
-    ascii_display mod_display(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`accline), .ex(`accline + 16), 
-        .sy(`ystart + 8 * 7), .ey(`ystart + 8 * 8), 
-        .ascii(6'h25), 
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_mod)
-    );
-    text_display wpmtext(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`wpmline), .ex(`wpmline + 16), 
-        .sy(`ystart), .ey(`ystart + 8 * 4), 
-        .text({5'd13, 5'd16, 5'd23}),
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_wpm_text)
-    );
-    num_display wpmdisplay(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`wpmline), .ex(`wpmline + 16), 
-        .sy(`ystart + 8 * 4), .ey(`ystart + 8 * 7), 
-        .num(wpm), .dot(0), 
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_wpm)
-    );
-    text_display timetext(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`timeline), .ex(`timeline + 16), 
-        .sy(`timeystart), .ey(`timeystart + 8 * 5), 
-        .text({5'd5, 5'd13, 5'd9, 5'd20}),
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_time_text)
-    );
-    num_display timedisplay(
-        .clk(clk), 
-        .valid(valid), 
-        .vgax(vgax), .vgay (vgay), 
-        .sx(`timeline), .ex(`timeline + 16), 
-        .sy(`timeystart + 8 * 5), .ey(`timeystart + 8 * 9), 
-        .num(times / 10), .dot(1), 
-        .font_color(4), .background_color(0), 
-        .pixel(pixel_time)
-    );
-
-    wire [2:0] pixel_id, pixel_id1;
-    find_max mx(
-        .a1(pixel_type), 
-        .a2(pixel_w0), 
-        .a3(pixel_w1), 
-        .a4(pixel_w2), 
-        .a5(pixel_w3), 
-        .a6(pixel_w4), 
-        .a7(pixel_acc), 
-        .a8(pixel_wpm), 
-        .mx(pixel_id1)
-    );
-    find_max mx2(
-        .a1(pixel_id1), 
-        .a2(pixel_wpm_text), 
-        .a3(pixel_acc_text), 
-        .a4(pixel_time), 
-        .a5(pixel_time_text), 
-        .a6(pixel_mod), 
-        .a7(0), 
-        .a8(0), 
-        .mx(pixel_id)
-    );
-
-    wire incar;
-    wire [11:0]car_addr;
-    assign incar = vgax >= `carx && vgax < `carx + 48 && vgay >= `cary + (percent * 4) && vgay < `cary + 80 + (percent * 4);
-    assign car_addr = incar ? ((vgax - `carx) * 80 + (vgay - `cary - (percent * 4))) : 0;
-    blk_mem_gen_0 clk_mem_gen_0_inst(
-        .clka(clk_25MHz), 
-        .wea(0), 
-        .addra(car_addr), 
-        .dina(0), 
-        .douta(pixel_car) 
-    );
-
-    always @ (posedge clk_25MHz) begin
-        if(incar) pixel <= pixel_car;
-        else if(vgax >= `carx + 52 && vgax < `carx + 56 && vgay >= `cary - 5 && vgay < `cary + 485) pixel <= 12'hFF0;
-        else begin
-            case(pixel_id)
-                0: pixel <= 12'hFFF; // white
-                1: pixel <= 12'h0FF; // blue
-                2: pixel <= 12'hF00; // red
-                3: pixel <= 12'h0F0; // green
-                4: pixel <= 12'd0;   // black
-                default: pixel <= 12'h00F; // error
-            endcase
-        end
-    end
-
-endmodule
-
-module max2(
-    input [2:0] a, b, 
-    output [2:0] mx
-);
-    assign mx = a > b ? a : b;
-endmodule
-module find_max(
-    input [2:0] a1, a2, a3, a4, a5, a6, a7, a8, 
-    output [2:0] mx
-);
-    wire [2:0] mx12, mx34, mx56, mx78, mx13, mx57;
-    max2(a1, a2, mx12);
-    max2(a3, a4, mx34);
-    max2(a5, a6, mx56);
-    max2(a7, a8, mx78);
-    max2(mx12, mx34, mx13);
-    max2(mx56, mx78, mx57);
-    max2(mx13, mx57, mx);
+//    assign pixel = pixel_game;
+    assign pixel = (state == SELECT ? pixel_menu : pixel_game);
+    assign {vgaRed, vgaGreen, vgaBlue} = valid ? pixel : 12'h0;
 endmodule
